4a4ef16881889bb66ac1c2cb8e71713ab3111a2d
Jane <ytzheng2003@gmail.com>
2023-02-02 15:28:03 -0500

    Added script for functions

---
+++ main.js
@@ -0,0 +1,9 @@
+function displayText(){
+
+	let buttonDiv = document.getElementById("moreText");
+	buttonDiv.innerHTML = "Visualization made using employee attrition dataset from Kaggle. \
+	Using Python to clean up dataset and split the dataset based on age-categories: Young, Middle, \
+	Senior, attrition rate for each group was calculated. Putting the calculated data into a dataframe, the dataframe\
+	was exported as a .csv file. The .csv file was imported into Tableau and a bar chart was created.";
+
+}
