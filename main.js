2f70de8fa43278c3b1d8994812f34c3b2234bb73
Jane <ytzheng2003@gmail.com>
2023-02-05 20:56:14 -0500

    Added button function to make text appear and disappear

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+let count = 0; 
+
 function displayText(){
-
+	count += 1;
 	let buttonDiv = document.getElementById("moreText");
-	buttonDiv.innerHTML = "Visualization made using employee attrition dataset from Kaggle. \
-		Using Python to clean up dataset and split the dataset based on age-categories: Young, Middle, \
-		Senior, attrition rate for each group was calculated. Putting the calculated data into a dataframe, the dataframe\
-		was exported as a .csv file. The .csv file was imported into Tableau and a bar chart was created.";
-
+	if (count % 2 == 0){
+		buttonDiv.innerHTML = "Visualization made using employee attrition dataset from Kaggle. \
+			Using Python to clean up dataset and split the dataset based on age-categories: Young, Middle, \
+			Senior, attrition rate for each group was calculated. Putting the calculated data into a dataframe, the dataframe\
+			was exported as a .csv file. The .csv file was imported into Tableau and a bar chart was created.";
+	}
+	else 
+		{buttonDiv.innerHTML = ""};
 }
